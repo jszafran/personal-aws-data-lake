7f10db67cc22fb6cf84c5b5ee89b77c49e61346e
jszafran <jszafran.pv@gmail.com>
2022-02-25 06:43:10 +0100

    Starting working on Glue table for Eurostat.

--- tf_infrastructure/glue.tf
+++ tf_infrastructure/glue.tf
@@ -1,3 +1,22 @@
+resource "aws_glue_catalog_database" "eurostat_database" {
+  name        = "eurostat_db"
+  description = "Database for data ingested from Eurostat"
+}
+
+resource "aws_glue_catalog_table" "eurostat_weekly_deaths" {
+  name          = "weekly_deaths"
+  database_name = aws_glue_catalog_database.eurostat_database.name
+  description   = "Data about weekly deaths (starting from 2000) sent by countries from EU."
+  table_type    = "EXTERNAL"
+  parameters = {
+    EXTERNAL = "TRUE"
+  }
+
+  storage_descriptor {
+    location = "s3://jszafran-data-lake/"
+  }
+}
+
 resource "aws_glue_job" "hello_world_glue_spark_job" {
   name              = "hello-world-glue-job"
   role_arn          = aws_iam_role.glue_data_lake_role.arn
