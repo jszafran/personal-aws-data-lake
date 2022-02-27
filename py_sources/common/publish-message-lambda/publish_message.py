bdfceee963f5bacd1f8f0376a13e8c9d3e2a8e10
jszafran <jszafran.pv@gmail.com>
2022-02-27 11:18:29 +0100

    Adding notification lambda layer.

---
+++ py_sources/common/publish-message-lambda/publish_message.py
@@ -0,0 +1,13 @@
+from notifications import SNSNotificationPublisher
+
+
+def lambda_handler(event, context):
+    topic_arn = event.get("topic_arn")
+    subject = event.get("subject")
+    message_body = event.get("message_body")
+
+    SNSNotificationPublisher.publish_message(
+        topic_arn=topic_arn,
+        subject=subject,
+        message_body=message_body,
+    )
